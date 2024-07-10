fc8c0cfe9e8f0a8ec01bb11348938c4e102495c2
Sujan Naik <sujannaik047@gmail.com>
2024-07-10 20:21:58 +0100

    Implement MongoDB support and API routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const TodoListRoutes = require('./routes/api/todoList')
+const path = require('path')
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+app.use(cors())
+app.use(bodyParser.json())
 
-app.listen(PORT, () => {
-    console.log(`App is listening at http://localhost:${PORT}`)
-})
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    })
+    .then(() => console.log('MongoDB database Connected...'))
+    .catch((err) => console.log(err))
+
+app.use('./routes/api/todoList', TodoListRoutes)
+
+app.listen(process.env.PORT, () => console.log(`App listening at http://localhost:${process.env.PORT}`))
