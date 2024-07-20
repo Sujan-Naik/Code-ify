f9d55c214dbd1ef082ed4c9b0263f26e402f522a
Sujan Naik <sujannaik047@gmail.com>
2024-07-20 11:49:09 +0100

    Enable switch between editor and preview

--- client/src/components/Showcase.vue
+++ client/src/components/Showcase.vue
@@ -1,47 +1,60 @@
 <script>
 
- import {useAuthStore} from "@/stores/auth.store.js";
-  import { storeToRefs } from 'pinia';
- import axios from "axios";
- import {router} from "@/main.js";
- import {computed, ref} from "vue";
- import UserPreviewList from "@/components/UserPreviewList.vue";
+import {useAuthStore} from "@/stores/auth.store.js";
+import axios from "axios";
+import {router} from "@/main.js";
+import {ref} from "vue";
+import UserPreviewList from "@/components/UserPreviewList.vue";
 
-import { marked } from 'marked';
+import {marked} from 'marked';
 
-export default{
+export default {
   components: {UserPreviewList},
   data() {
     return {
-      username : useAuthStore().user?.value?.username,
-      showcaseName : router.currentRoute.value.params.showcase,
-      showcaseBody : ref(''),
-      showcaseCreated : ref(''),
-      showcaseUsers : ref(Array),
+      username: useAuthStore().user?.username,
+      showcaseName: router.currentRoute.value.params.showcase,
+      showcaseBody: ref(''),
+      showcaseCreated: ref(''),
+      showcaseUsers: ref(Array),
+      isEditor: false,
+      isAdmin: false
     }
   },
-  methods:{
-    async getShowcase(){
+  methods: {
+    async getShowcase() {
       await axios.get("http://localhost:3000/api/showcase/", {
-       params: {
-         name: this.showcaseName
-       }
-     }).then(value => {
-       this.showcaseBody = value.data.contents
-       this.showcaseCreated = value.data.createdAt
-       this.showcaseUsers.value = value.data.users
-     }).catch(reason => {
-       console.log(reason)
-     })
+        params: {
+          name: this.showcaseName
+        }
+      }).then(value => {
+        this.showcaseBody = value.data.contents
+        this.showcaseCreated = value.data.createdAt
+        this.showcaseUsers.value = value.data.users
+        for (let valueKey in this.showcaseUsers.value) {
+          if (this.showcaseUsers.value[valueKey].username === this.username) {
+            this.isEditor = true;
+            this.isAdmin = true;
+          }
+        }
+      }).catch(reason => {
+        console.log(reason)
+      })
     },
-    renderMarkdown(){
-       const html = marked.parse(this.showcaseBody);
-       this.$refs.renderedMD.innerHTML = html
+    renderMarkdown() {
+      const html = marked.parse(this.showcaseBody);
+      this.$refs.renderedMD.innerHTML = html
+    },
+    togglePreview() {
+      this.isEditor = !this.isEditor
+      this.renderMarkdown
     }
   },
   beforeMount() {
     this.getShowcase()
-
+  },
+  mounted() {
+    this.renderMarkdown()
   }
 }
 
@@ -50,11 +63,41 @@ export default{
 
 <template>
 
-    <h1> This is {{showcaseName}}</h1>
-    <a> {{showcaseCreated }}</a>
-    <textarea v-model="showcaseBody" @input="renderMarkdown"></textarea>
-    <div class="card" ref="renderedMD">
+  <h1> This is {{ showcaseName }}</h1>
+  <a> {{ showcaseCreated }}</a>
+  <UserPreviewList :userList="showcaseUsers.value"></UserPreviewList>
+  <div v-if="isAdmin">
+    <button @click="togglePreview">Toggle preview</button>
+
+      <div class="row justify-content-center">
+        <div class="col-6" v-show="isEditor">
+          <div class="card">
+            <textarea v-model="showcaseBody" @input="renderMarkdown"></textarea>
+          </div>
+        </div>
+        <div class="col-6">
+          <div ref="renderedMD" class="card">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  <div v-if="!isAdmin">
+    <div ref="renderedMD" class="card">
 
     </div>
-    <UserPreviewList :userList="showcaseUsers.value"></UserPreviewList>
+  </div>
+
 </template>
+
+
+
+
+
+
+
+
+
+
+
